e157639cb71e53bbdb767a6c96463bfff28ec68c
Sammie (Ella) Chung <sc6aa@virginia.edu>
2024-10-26 18:44:58 -0400

    Resource for creating subnet, vnet, NSG, and inbound rule

--- az-linux-vm/main.tf
+++ az-linux-vm/main.tf
@@ -23,3 +23,43 @@ resource "azure_vm" "virtualm" {
     # The following is injecting CloudInit directly using Tailscale resource using template tftpl
     userdata = templatefile("azuredeployment.tftpl", { tailscale_auth_key = var.tailscale_auth_key})
 }
+
+# Creating a vnet
+resource "azurerm_virtual_network" "takehome" {
+    name = "takehome-vnet"
+    location = azurerm_resource_group.takehome.location
+    resource_group_name = azurerm_resource_group.takehome.name
+    address_space = ["10.0.0.0/24"]
+}
+
+# Creating a subnet
+resource "azurerm_subnet" "takehome" {
+    name = "takehome-subnet"
+    resource_group_name = azurerm_resource_group.takehome.name
+    virtual_network_name = azurerm_virtual_network.takehome.name
+    address_prefixes = ["10.0.0.0/24"]
+}
+
+#Creating a NSG
+resource "azurerm_network_security_group" "takehome_nsg" {
+    name = "takehome-nsg"
+    location = azurerm_resource_group.takehome.location
+    resource_group_name = azurerm_resource_group.takehome.name
+}
+
+# Set inbound security rule
+resource "azurerm_network_security_rule" "takehome-nsg" {
+    name = "Tailscale"
+    description = "Tailscale UDP port"
+    priority = 1010
+    direction = "Inbound"
+    access = "Allow"
+    protocol = "UDP"
+    source_port_range = "*"
+    destination_port_range = 41641
+    source_address_prefix = "*"
+    destination_address_prefix = "*"
+    resource_group_name = azurerm_resource_group.takehome.name
+    network_security_group_name = azurerm_network_security_group.takehome_nsg.name
+}
+
